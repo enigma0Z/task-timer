177d91d2e88d74b0898d5b10e20543c23ab429b1
Johnathan Bell <enigma.0ZA@gmail.com>
2020-11-02 20:43:19 -0500

    Update label slider UI

--- src/components/label-slider/index.tsx
+++ src/components/label-slider/index.tsx
@@ -1,11 +1,39 @@
-import { createStyles, Slider, Theme, WithStyles, withStyles } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography/Typography";
 import React, { Component } from "react";
 
+import {
+    Box,
+    IconButton,
+    Menu,
+    MenuItem,
+    Slider,
+    Theme,
+    Typography,
+    WithStyles, withStyles, createStyles,
+} from "@material-ui/core";
+
+import SettingsIcon from '@material-ui/icons/Settings';
+
 const styles = (theme: Theme) => createStyles({
+    fullWidth: {
+        width: '100%'
+    },
+
+    centeringGrid: {
+        display: 'flex'
+    },
+
+    listItemText: {
+        display: 'block'
+    },
+
     sliderSpacing: {
-        paddingLeft: 8,
-        paddingRight: 8,
+        width: '100%',
+        justifyContent: 'center',
+        verticalAlign: 'middle'
+    },
+
+    sliderLabel: {
+        paddingRight: 10
     }
 })
 
@@ -16,18 +44,22 @@ interface LabelSliderProps extends WithStyles<typeof styles> {
     step: number,
     min: number,
     max: number,
-    onChange: Function
+    onChange: Function,
+    formatCallback: Function
 }
 
 interface LabelSliderState {
     value: number
+    menuOpen: boolean
+    anchor?: HTMLButtonElement
 }
 
 export const LabelSlider = withStyles(styles)(class LabelSliderComponent extends Component<LabelSliderProps, LabelSliderState> {
     constructor(props: LabelSliderProps) {
         super(props)
         this.state = {
-            value: this.props.value
+            value: this.props.value,
+            menuOpen: false
         }
 
         this.handleOnChange = this.handleOnChange.bind(this)
@@ -50,22 +82,49 @@ export const LabelSlider = withStyles(styles)(class LabelSliderComponent extends
 
     render() {
         const { classes } = this.props
-        return(
-            <div>
-                <Typography variant="caption" id='break-length' gutterBottom>{this.props.label} ({this.state.value}{this.props.labelSuffix})</Typography>
-                <div className={classes.sliderSpacing}>
-                    <Slider
-                        value={this.state.value}
-                        step={this.props.step}
-                        min={this.props.min}
-                        max={this.props.max}
-                        onChange={this.handleOnChange}
-                        getAriaValueText={(value: number) => {return `${value}m`}}
-                        valueLabelDisplay="off"
-                        aria-labelledby="break-length"
-                    />
-                </div>
-            </div>
+        return (
+            <Box display='flex' flexDirection='column'>
+                <Menu
+                    id="simple-menu"
+                    keepMounted
+                    open={this.state.menuOpen}
+                    anchorEl={this.state.anchor}
+                    onClose={() => {
+                        this.setState({ menuOpen: false })
+                    }}
+                >
+                    <MenuItem>Edit</MenuItem>
+                    <MenuItem>Delete</MenuItem>
+                </Menu>
+                <Box display='flex' flexDirection='row' flex='100%' alignItems='baseline'>
+                    <Typography variant="subtitle1" className={classes.sliderLabel}>{this.props.label}</Typography>
+                    <Typography variant="caption" className={classes.sliderLabel}>({this.props.formatCallback(this.state.value)})</Typography>
+                </Box>
+                <Box display='flex' flex={1} flexDirection='row'>
+                    <Box m={1} flex='100%' display='flex' justifyContent='flex-start' alignItems='center'>
+                        <Slider
+                            value={this.state.value}
+                            step={this.props.step}
+                            min={this.props.min}
+                            max={this.props.max}
+                            onChange={this.handleOnChange}
+                            getAriaValueText={(value: number) => { return `${value}m` }}
+                            valueLabelDisplay="off"
+                            aria-labelledby="break-length"
+                        />
+                    </Box>
+                    <Box flex={1} display='flex' justifyContent='flex-end' alignItems='center'>
+                        <IconButton onClick={(event) => {
+                            this.setState({
+                                anchor: event.currentTarget,
+                                menuOpen: true
+                            })
+                        }}>
+                            <SettingsIcon />
+                        </IconButton>
+                    </Box>
+                </Box>
+            </Box>
         )
     }
 });
