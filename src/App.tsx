5ef1d46743e31c12acdbc64ec6317d68b0ca76d6
Johnathan Bell <enigma.0ZA@gmail.com>
2020-11-08 13:13:34 -0500

    Use newly implemented classes and components
    * NotificationService
    * Countdown class
    * Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,24 @@ import React, { Component } from 'react';
 import {
     AppBar,
     createStyles,
-    Drawer,
     Grid,
     IconButton,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemSecondaryAction,
-    ListItemText,
     Paper,
-    Switch,
     Theme,
     Toolbar,
     Typography,
     WithStyles, withStyles
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import NotificationsIcon from '@material-ui/icons/Notifications';
 import Button from '@material-ui/core/Button/Button';
 
 import './App.css';
 import { LabelSlider } from './components/label-slider';
-import { Countdown } from './data/model/countdown'
+import { Sidebar } from './components/sidebar';
+import { Countdown, CountdownJsonObject } from './data/Countdown'
 import { TimeFormat } from './data/format/time'
+
+import { NotificationService } from './services/notification'
 
 const styles = (theme: Theme) => createStyles({
     root: {
@@ -57,9 +52,6 @@ const styles = (theme: Theme) => createStyles({
         paddingTop: 16
     },
 
-    sideBar: {
-        width: 250
-    }
 });
 
 interface AppProps extends WithStyles<typeof styles> { }
@@ -69,7 +61,7 @@ interface AppState {
     breakLength: number,
     running: boolean,
     secondsLeft: number,
-    currentCountdown: Countdown,
+    currentCountdownIndex: number,
     countdowns: Countdown[],
     sidebarOpen: boolean,
     notificationSupport: boolean,
@@ -82,13 +74,9 @@ const DEFAULT_COUNTDOWNS: Countdown[] = [
     new Countdown('Break time', 1, 15, 10),
 ]
 
-DEFAULT_COUNTDOWNS[0].setNext(DEFAULT_COUNTDOWNS[1])
-DEFAULT_COUNTDOWNS[1].setNext(DEFAULT_COUNTDOWNS[0])
+const notificationService: NotificationService = NotificationService.instance
 
 const App = withStyles(styles)(class AppComponent extends Component<AppProps, AppState> {
-    private timer: any;
-    public notification: any;
-
     constructor(props: AppProps) {
         super(props)
 
@@ -100,109 +88,76 @@ const App = withStyles(styles)(class AppComponent extends Component<AppProps, Ap
             sidebarOpen: false,
             notificationSupport: "Notification" in window,
             countdowns: DEFAULT_COUNTDOWNS,
-            currentCountdown: DEFAULT_COUNTDOWNS[0]
+            currentCountdownIndex: 0
         }
 
-        this.getButtonText = this.getButtonText.bind(this)
         this.handleStartStopOnClick = this.handleStartStopOnClick.bind(this)
-        this.updateSecondsLeft = this.updateSecondsLeft.bind(this)
-        this.requestNotificationPermission = this.requestNotificationPermission.bind(this)
+        this.updateCountdownState = this.updateCountdownState.bind(this)
+        this.updateSubscriber = this.updateSubscriber.bind(this)
     }
 
+    get currentCountdown(): Countdown {
+        return this.state.countdowns[this.state.currentCountdownIndex]
+    }
 
-
-    getButtonText() {
-        if (this.state.running) {
-            return "Stop"
-        } else {
-            return "Start"
-        }
+    get nextCountdownIndex(): number {
+        return (this.state.currentCountdownIndex + 1) % this.state.countdowns.length
     }
 
     getNextCountdown(): Countdown {
-        if (this.state.currentCountdown.next !== undefined) {
-            return this.state.currentCountdown.next
+        return this.state.countdowns[this.nextCountdownIndex]
+    }
+
+    updateCountdownState(countdown: Countdown) {
+        this.setState({
+            secondsLeft: countdown.secondsLeft,
+            running: countdown.running
+        })
+
+        localStorage.setItem(
+            'countdowns',
+            JSON.stringify(
+                this.state.countdowns.map((countdown) => countdown.exportToJsonObject())
+            )
+        )
+    }
+
+    updateSubscriber() {
+        this.updateCountdownState(this.currentCountdown)
+
+        if (!this.currentCountdown.running) {
+            notificationService.showNotification(
+                APP_TITLE,
+                {
+                    body: `${this.currentCountdown.name} done`
+                }
+            )
+            this.currentCountdown.unsubscribe(this.constructor.name)
         }
-
-        return this.state.currentCountdown
-    }
-
-    updateSecondsLeft() {
-        if (this.state.endTime !== undefined) {
-            let newSecondsLeft = (this.state.endTime - Date.now()) / 1000
-            if (this.state.running && newSecondsLeft > 0) {
-                this.setState({
-                    secondsLeft: Math.floor(newSecondsLeft)
-                })
-
-                localStorage.setItem('secondsLeft', Math.floor(newSecondsLeft).toString())
-
-                this.setUpdateTimer()
-            } else {
-                this.stopTimer()
-            }
-        } else {
-            return 0
-        }
-    }
-
-    setUpdateTimer() {
-        this.timer = setTimeout(() => { this.updateSecondsLeft() }, 1000)
-    }
-
-    clearUpdateTimer() {
-        clearTimeout(this.timer)
     }
 
     startTimer() {
-        let now = Date.now()
-        let newEndTime = now + (this.state.currentCountdown.value * 60 * 1000)
-        let newSecondsLeft = this.state.currentCountdown.value * 60
-
-        this.setState({
-            running: true,
-            endTime: newEndTime,
-            secondsLeft: newSecondsLeft
-        })
-
-        localStorage.setItem('running', true.toString())
-        localStorage.setItem('endTime', newEndTime.toString())
-        localStorage.setItem('secondsLeft', newSecondsLeft.toString())
-        localStorage.setItem('currentCountdownName', this.state.currentCountdown.name)
-
-        this.setUpdateTimer()
+        this.currentCountdown.subscribe(this.constructor.name, this.updateSubscriber)
+        this.currentCountdown.start()
     }
 
-    stopTimer(showNotification: boolean = true) {
-        if (showNotification && this.state.notificationSupport) {
-            let notification = new Notification(
-                APP_TITLE,
-                {
-                    body: `${this.state.currentCountdown.name} completed!`,
-                }
-            )
-
-            notification.onclick = (event: Event) => { notification.close(); window.focus() }
-        }
+    stopTimer() {
+        this.currentCountdown.unsubscribe(this.constructor.name)
+        this.currentCountdown.stop()
+        this.updateCountdownState(this.currentCountdown)
 
         this.setState({
-            running: false,
-            secondsLeft: 0,
-            currentCountdown: this.getNextCountdown()
+            currentCountdownIndex: this.nextCountdownIndex
         })
 
-        localStorage.setItem('running', false.toString())
-        localStorage.setItem('secondsLeft', '0')
-        localStorage.setItem('currentCountdownName', this.getNextCountdown().name)
-
-        this.clearUpdateTimer()
+        localStorage.setItem('currentCountdownIndex', this.nextCountdownIndex.toString())
     }
 
     handleStartStopOnClick() {
         if (!this.state.running) {
             this.startTimer()
         } else {
-            this.stopTimer(false)
+            this.stopTimer()
         }
     }
 
@@ -225,35 +180,34 @@ const App = withStyles(styles)(class AppComponent extends Component<AppProps, Ap
         )
     }
 
-    requestNotificationPermission() {
-        if (this.state.notificationSupport) {
-            if (Notification.permission === 'default') {
-                Notification.requestPermission()
-            } else if (Notification.permission === 'denied') {
-                alert('Notifications have been disabled, enable them in your browser')
-            }
-        }
-    }
-
     componentDidMount() {
-        this.requestNotificationPermission()
+        notificationService.requestDesktopNotificationPermissions()
 
         // Load from state
-        const running = localStorage.getItem('running') === 'true'
-        const endTime = localStorage.getItem('endTime')
-        const secondsLeft = localStorage.getItem('secondsLeft')
-        const currentCountdown = this.state.countdowns.find((countdown: Countdown) => { return countdown.name === localStorage.getItem('currentCountdownName') })
+        const currentCountdownIndex = localStorage.getItem('currentCountdownIndex')
+        const countdownsStr = localStorage.getItem('countdowns')
+
+        if (countdownsStr !== null) {
+            const countdowns = JSON.parse(countdownsStr).map((countdownJsonObject: CountdownJsonObject) => {
+                return new Countdown().loadFromJsonObject(countdownJsonObject)
+            })
+
+            this.setState({
+                countdowns: countdowns,
+            })
+
+            const runningCountdown: Countdown = countdowns.filter((countdown: Countdown) => {return countdown.running})[0]
+
+            if (runningCountdown) {
+                runningCountdown.update()
+                runningCountdown.subscribe(this.constructor.name, this.updateSubscriber)
+                this.updateCountdownState(runningCountdown)
+            }
+        }
 
         this.setState({
-            running: running,
-            endTime: endTime ? parseInt(endTime) : this.state.endTime,
-            secondsLeft: secondsLeft ? parseInt(secondsLeft) : this.state.secondsLeft,
-            currentCountdown: currentCountdown ? currentCountdown : this.state.currentCountdown
+            currentCountdownIndex: currentCountdownIndex ? parseInt(currentCountdownIndex) : this.state.currentCountdownIndex
         })
-
-        if (running) {
-            this.setUpdateTimer()
-        }
     }
 
     render() {
@@ -266,7 +220,9 @@ const App = withStyles(styles)(class AppComponent extends Component<AppProps, Ap
                             edge="start"
                             className={classes.menuButton}
                             color="inherit" aria-label="menu"
-                            onClick={() => { this.setState({ sidebarOpen: true }) }}
+                            onClick={() => {
+                                this.setState({ sidebarOpen: true })
+                            }}
                         >
                             <MenuIcon />
                         </IconButton>
@@ -275,32 +231,12 @@ const App = withStyles(styles)(class AppComponent extends Component<AppProps, Ap
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <Drawer
-                    anchor='left'
-                    open={this.state.sidebarOpen}
-                    onClose={() => { this.setState({ sidebarOpen: false }) }}
-                >
-                    <div className={classes.sideBar} >
-                        <List>
-                            <ListItem
-                                button
-                                onClick={this.requestNotificationPermission}
-                            >
-                                <ListItemIcon><NotificationsIcon /></ListItemIcon>
-                                <ListItemText primary="Notifications" />
-                                <ListItemSecondaryAction>
-                                    <Switch
-                                        edge="end"
-                                        onClick={this.requestNotificationPermission}
-                                        checked={this.state.notificationSupport && Notification.permission === 'granted'}
-                                        inputProps={{ 'aria-labelledby': 'switch-list-label-bluetooth' }}
-                                        disabled={this.state.notificationSupport === false}
-                                    />
-                                </ListItemSecondaryAction>
-                            </ListItem>
-                        </List>
-                    </div>
-                </Drawer>
+                <Sidebar
+                    onOpen={() => this.state.sidebarOpen}
+                    onClose={() => {
+                        this.setState({sidebarOpen: false})
+                    }}
+                />
                 <Grid container className={classes.gridContainer} spacing={2}>
                     <Grid item xs={12} lg={2}> <Paper className={classes.paperContainer}>
                         <Grid container direction="column" spacing={2}>
@@ -314,14 +250,14 @@ const App = withStyles(styles)(class AppComponent extends Component<AppProps, Ap
                             </Grid>
                             <Grid item>
                                 <Button variant="contained" className={classes.fillWidth} onClick={this.handleStartStopOnClick}>
-                                    {this.getButtonText()}
+                                    {this.state.running ? 'Stop' : 'Start'}
                                 </Button>
                             </Grid>
                         </Grid>
                     </Paper> </Grid>
                     <Grid item xs>
                         <Paper className={classes.paperContainer}>
-                            <Typography variant="h5">{this.state.running ? "Running" : "On deck"}: {this.state.currentCountdown.name}</Typography>
+                            <Typography variant="h5">{this.state.running ? "Running" : "On deck"}: {this.currentCountdown.name}</Typography>
                             <Typography variant="h6">Time left {TimeFormat.seconds(this.state.secondsLeft)}</Typography>
                             <Typography variant="caption">Up next: {this.getNextCountdown().name}</Typography>
                         </Paper>
