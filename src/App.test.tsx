c70b4e5d6e41cf2938ce43d56a2e3735bcaec05e
Johnathan Bell <enigma.0ZA@gmail.com>
2020-11-22 15:08:39 -0500

    Fix broken tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders on deck text', () => {
+test('renders the timer status bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/On deck: .*/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/On deck/i)).toBeInTheDocument();
+  expect(screen.getByText(/Time left/i)).toBeInTheDocument();
+  expect(screen.getByText(/Up next/i)).toBeInTheDocument();
 });
