e7694bf2abcdef90144cd4e9746e73f7fa472e0a
Johnathan Bell <enigma.0ZA@gmail.com>
2021-01-17 22:54:05 -0500

    Comment out debug logging statements

--- src/data/Subscribable.ts
+++ src/data/Subscribable.ts
@@ -3,18 +3,18 @@ export abstract class Subscribable {
     // abstract readonly name: string
 
     public subscribe(id: string, callback: () => void) {
-        console.log('Subscribe', id, 'to', this.constructor.name, this)
+        // console.log('Subscribe', id, 'to', this.constructor.name, this)
         this.subscribers.set(id, callback)
     }
 
     public unsubscribe(id: string) {
-        console.log('Unsubscribe', id, 'from', this.constructor.name, this)
+        // console.log('Unsubscribe', id, 'from', this.constructor.name, this)
         this.subscribers.delete(id)
     }
 
     protected updateSubscribers() {
         this.subscribers.forEach((value, key, map) => {
-            console.log('Update Subscriber', key, 'on', this.constructor.name, this)
+            // console.log('Update Subscriber', key, 'on', this.constructor.name, this)
             value()
         })
     }
